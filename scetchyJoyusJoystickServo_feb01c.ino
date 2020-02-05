6240c8b9f7bdcef0863c918b30f141afbc9d86ed
WhitmoreLakeTroBots <wlhsroboticsteam@gmail.com>
2020-02-05 18:54:10 -0500

    HF First commit

---
+++ scetchyJoyusJoystickServo_feb01c.ino
@@ -0,0 +1,107 @@
+#include<Servo.h>
+Servo servo1;
+Servo servo2;
+int x_key = A1;
+int y_key = A0;
+int x_pos;
+int y_pos;
+int servo1_pin = 8;
+int servo2_pin = 9;
+int initial_position = 90;
+int initial_position1 = 90;
+
+void setup() {
+  Serial.begin (9600) ;
+  servo1.attach (servo1_pin ) ;
+  servo2.attach (servo2_pin ) ;
+  servo1.write (initial_position);
+  servo1.write (initial_position1);
+  pinMode (x_key, INPUT) ;
+  pinMode (y_key, INPUT) ;
+  Serial.write("start");
+}
+
+void loop() {
+  x_pos = analogRead (x_key) ;
+  y_pos = analogRead (y_key) ;
+/*
+if (300 > x_pos < 700){
+  initial_position = 90;
+      servo2.write(initial_position);
+      Serial.print(initial_position);
+      Serial.println();
+      delay(100);
+}
+  if (x_pos < 300) {
+    if (initial_position < 10) {
+    }
+    else
+    { initial_position = initial_position - 20;
+      servo1.write(initial_position);
+      Serial.write("x-20");
+      Serial.println();
+      Serial.print(initial_position);
+      Serial.println();
+      delay(100);
+      initial_position = initial_position + 20;
+      servo2.write(initial_position);
+      Serial.print(initial_position);
+      Serial.println();
+      delay(100);
+      }
+  }
+  if (x_pos > 700) {
+    if ( initial_position > 180) {
+    }
+    else {
+      initial_position = initial_position + 20;
+      servo1.write ( initial_position ) ;
+      Serial.write("x+20");
+      Serial.println();
+      Serial.print(initial_position);
+      Serial.println();
+      delay (100) ;
+      initial_position = initial_position - 20;
+     servo2.write(initial_position);
+      Serial.print(initial_position);
+      Serial.println();
+      delay(100);
+    }
+  }*/
+if (300 > y_pos < 700){
+  /*if(initial_position1 == -10){
+    }else{*/initial_position1 = 90;
+      servo2.write(initial_position1);
+      Serial.print(initial_position1);
+      Serial.write("stop");
+      Serial.println();
+      delay(100);
+  
+}
+  if (y_pos < 300) {
+    if (initial_position1 < 10) {
+    }
+    else {
+      initial_position1 = initial_position1 - 20;
+      servo2.write(initial_position1);
+      Serial.write("Y-20");
+      Serial.println();
+      Serial.print(initial_position1);
+      Serial.println();
+      delay(100);
+    }
+  }
+  if (y_pos > 700) {
+    if ( initial_position1 > 180) {
+    }
+    else {
+      initial_position1 = initial_position1 + 20;
+      servo2.write ( initial_position1 ) ;
+      Serial.write("Y-20");
+      Serial.println();
+      Serial.print(initial_position1);
+      Serial.println();
+      delay (100) ;
+    }
+  }
+}
